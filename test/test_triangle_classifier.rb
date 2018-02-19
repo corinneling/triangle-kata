b2e7f5d09976c0827d859eb6333cfc49f453d467
corinneling <corinne@heysparkbox.com>
2018-02-19 15:11:31 -0500

    add TriangleClassifier and #test_does_triangle_classifier_exist

--- test/test_triangle_classifier.rb
+++ test/test_triangle_classifier.rb
@@ -1,2 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require './triangle-classification/triangle_classifier'
+
 class TestTriangleClassifier < Minitest::Test
-end 
+
+  def setup
+    @triclass = TriangleClassifier.new
+  end
+
+  def test_does_triangle_classifier_exist
+    assert_instance_of TriangleClassifier, @triclass
+  end
+
+end
---
+++ triangle-classification/triangle_classifier.rb
@@ -0,0 +1,2 @@
+class TriangleClassifier
+end
