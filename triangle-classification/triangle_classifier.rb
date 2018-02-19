309e38a9d675201683afab64d65ccf22121c0c97
corinneling <corinne@heysparkbox.com>
2018-02-19 16:44:03 -0500

    move item in comments
    from incomplete comments section
    to completed comments sections

--- triangle-classification/triangle_classifier.rb
+++ triangle-classification/triangle_classifier.rb
@@ -2,11 +2,11 @@
 # Create a class TriangleClassifier with the following method:
 # classify(len1, len2, len3)
 
-# -- incomplete -- #
 # This method accepts three numeric values each representing the length of one
 # side of a triangle. It returns a symbol, :scalene, :isosceles, or :equilateral,
 # where the return value corresponds to the actual type of the triangle.
 
+# -- incomplete -- #
 # In addition, the classify method should perform error checking if the given
 # lengths of the sides cannot correspond to a real triangle. In this case, it
 # should raise the error ArgumentError.
